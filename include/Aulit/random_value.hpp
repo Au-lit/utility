447a8ed5de3114108cab4de613bf7ec56942ce56
Olliver Roberge <ollivier.roberge@gmail.com>
2021-04-27 17:50:28 -0400

    Reseed uniform_random_value.

--- include/Aulit/random_value.hpp
+++ include/Aulit/random_value.hpp
@@ -5,24 +5,33 @@
 #include <limits>
 #include <random>
 #include <type_traits>
+#include <cstddef>
 
 namespace Aulit {
-    template<typename ResultType>
+	enum eReseedEngine {};
+	template<typename ResultType>
 #if __has_cpp_attribute(nodiscard) >= 201603L
-    [[nodiscard]]
+	[[nodiscard]]
 #endif
-    ResultType uniform_random_value(
-        ResultType a = 0,
-        ResultType b = std::numeric_limits<ResultType>::max()
-    ) noexcept {
-        thread_local std::random_device rd;
-        thread_local std::default_random_engine random_engine(rd());
-        if constexpr (std::is_floating_point<ResultType>::value)
-            return std::uniform_real_distribution<ResultType>(a, b).operator()(random_engine);
-        else if constexpr (std::is_same<ResultType, bool>::value)
-            return std::bernoulli_distribution().operator()(random_engine);
-        else return std::uniform_int_distribution<ResultType>(a, b).operator()(random_engine);
-    }
+	ResultType uniform_random_value(
+		ResultType a = static_cast<ResultType>(0),
+		ResultType b = static_cast<ResultType>(std::numeric_limits<ResultType>::max())
+	) noexcept {
+		thread_local std::random_device rd;
+		thread_local std::default_random_engine random_engine(rd());
+		if constexpr (std::is_same<ResultType, eReseedEngine>::value) {
+			const auto seed = rd();
+			random_engine.seed(seed);
+			return static_cast<ResultType>(seed);
+		}
+		else {
+			if constexpr (std::is_floating_point<ResultType>::value)
+				return std::uniform_real_distribution<ResultType>(a, b).operator()(random_engine);
+			else if constexpr (std::is_same<ResultType, bool>::value)
+				return std::bernoulli_distribution().operator()(random_engine);
+			else return std::uniform_int_distribution<ResultType>(a, b).operator()(random_engine);
+		}
+	}
 }
 
 #endif // !defined(RANDOM_VALUE_IMPL)
