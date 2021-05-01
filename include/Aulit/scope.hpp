3c9caaf09d2447d0a04398b967efb1b4a99d8bf4
Olliver Roberge <ollivier.roberge@gmail.com>
2021-04-30 22:10:03 -0400

    Changes to scope.hpp to make it more convivial to use.

--- include/Aulit/scope.hpp
+++ include/Aulit/scope.hpp
@@ -1,4 +1,7 @@
 // © Copyright 2021 Ollivier Roberge
+#ifndef SCOPE_EXIT_IMPL
+#define SCOPE_EXIT_IMPL
+
 #include <type_traits>
 #include <functional>
 #include <exception>
@@ -10,12 +13,13 @@ namespace Aulit {
 		inline namespace experimental {
 			template<typename Ef> class scope_exit {
 			public:
+				constexpr scope_exit() noexcept = default;
 				template<typename Fn>
 				constexpr scope_exit(Fn&& function) noexcept(std::is_nothrow_constructible_v<Fn>)
 					: m_function(std::forward<Fn>(function)) {}
 				constexpr scope_exit(scope_exit&& other) noexcept = default;
 				scope_exit(const scope_exit& other) = delete;
-				scope_exit& operator=(scope_exit&&) = delete;
+				scope_exit& operator=(scope_exit&&) = default; // this is not standard but useful
 				scope_exit& operator=(const scope_exit&) = delete;
 				constexpr ~scope_exit() noexcept { if (m_canExit) std::invoke(m_function); }
 				constexpr void release() noexcept { m_canExit = false; }
@@ -26,12 +30,13 @@ namespace Aulit {
 
 			template<typename Ef> class scope_fail {
 			public:
+				constexpr scope_fail() noexcept = default;
 				template<typename Fn>
 				constexpr scope_fail(Fn&& function) noexcept(std::is_nothrow_constructible_v<Fn>)
 					: m_function(std::forward<Fn>(function)) {}
 				constexpr scope_fail(scope_fail&& other) noexcept = default;
 				scope_fail(const scope_fail& other) = delete;
-				scope_fail& operator=(scope_fail&&) = delete;
+				scope_fail& operator=(scope_fail&&) = default;
 				scope_fail& operator=(const scope_fail&) = delete;
 				constexpr ~scope_fail() noexcept { if (m_canExit && std::uncaught_exceptions() > 0) std::invoke(m_function); }
 				constexpr void release() noexcept { m_canExit = false; }
@@ -42,12 +47,13 @@ namespace Aulit {
 
 			template<typename Ef> class scope_success {
 			public:
+				constexpr scope_success() noexcept = default;
 				template<typename Fn>
 				constexpr scope_success(Fn&& function) noexcept(std::is_nothrow_constructible_v<Fn>)
 					: m_function(std::forward<Fn>(function)) {}
 				constexpr scope_success(scope_success&& other) noexcept = default;
 				scope_success(const scope_success& other) = delete;
-				scope_success& operator=(scope_success&&) = delete;
+				scope_success& operator=(scope_success&&) = default;
 				scope_success& operator=(const scope_success&) = delete;
 				constexpr ~scope_success() noexcept { if (m_canExit && std::uncaught_exceptions() < 1) std::invoke(m_function); }
 				constexpr void release() noexcept { m_canExit = false; }
@@ -88,3 +94,5 @@ namespace Aulit {
 		}
 	}
 }
+
+#endif // !SCOPE_EXIT_IMPL
